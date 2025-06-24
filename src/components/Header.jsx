deeae3fc78b9a7973aa7fbfd8be47e42f1193b43
WilliamArbash516 <90899976+William5162A@users.noreply.github.com>
2025-06-25 02:26:32 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className={'flex flex-col items-center select-none'}>
+    <div className={'flex flex-col items-center select-none mt-6'}>
       <h1 className={'text-blue-600 text-[16px] lg:text-4xl font-bold'}>
           Task Manager
       </h1>
